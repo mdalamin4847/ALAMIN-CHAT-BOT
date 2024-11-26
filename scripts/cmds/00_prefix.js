6ec70e4f171ad2b76353d01f271daf2d597933fb
Md Alamin Chowdhury <malamin4847@gmail.com>
2024-11-26 20:43:05 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -17,23 +17,22 @@ module.exports = {
  body: `
 Yo, my prefix is [╔╝❮ ❯╚╗
 ━━━━━━━━━━━━━━━━━━━━━━
-𝗡𝗔𝗠𝗘: BOSS~ASHIK
+𝗡𝗔𝗠𝗘:𝐀𝐋𝐀𝐌𝐈𝐍
 𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍: 𝐈𝐒𝐋𝐀𝐌
-𝐀𝐃𝐃𝐑𝐄𝐒𝐒: 𝗧𝗔𝗥𝗔𝗞𝗔𝗡𝗗𝗜, 𝗦𝗔𝗥𝗜𝗦𝗛𝗔𝗕𝗔𝗥𝗜, 𝗝𝗔𝗠𝗔𝗟𝗣𝗨𝗥
+𝐀𝐃𝐃𝐑𝐄𝐒𝐒: 𝗜𝗦𝗛𝗔𝗟𝗠𝗔𝗥𝗜, 𝗠𝗜𝗥𝗣𝗨𝗥, 𝗞𝗨𝗦𝗛𝗧𝗜𝗔
 𝐆𝐄𝐍𝐃𝐄𝐑: 𝐌𝐀𝐋𝐄
 𝐀𝐆𝐄: 17+
 𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏: 𝗦𝗜𝗡𝗚𝗟𝗘
 𝐖𝐎𝐑𝐊: 𝗦𝗧𝗨𝗗𝗘𝗡𝗧
-𝐆𝐌𝐀𝐈𝐋: bossagor745@gmail.com
-𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/profile.php?id=100095089282395&mibextid=ZbWKwL 
+𝐆𝐌𝐀𝐈𝐋: mdalaminhosen4847@gmail.com
+𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://facebook.com/MRALAMINXHOWDHURY
 𝐌𝐀𝐒𝐒𝐄𝐍𝐆𝐄𝐑:  পেম করলে দিবো 😛
-𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+881647711001
-𝐈𝐌𝐎: 01647711001
-𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: @DXAshik
+𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: https://t.me/mdalaminxhowdhury
+𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+8801748473269
 ━━━━━━━━━━━━━━━━━━━━━━
 
 Bot Prefix: ( / )
-Bot Name: ASHIK~BOT]`,
+Bot Name: 𝐌𝐝 𝐀𝐥𝐚𝐦𝐢𝐧 𝐂𝐡𝐨𝐰𝐝𝐡𝐮𝐫𝐲]`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/hwLP4lF.mp4")
  });
  }
